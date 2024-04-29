b395c80daeb930828e67c7c7ae1ae82b9cd1591d
Scott Nath <github@scottnath.com>
2024-04-29 19:55:10 -0400

    :shirt: moar cleanup

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,9 +1,14 @@
 import type { StorybookConfig } from '@storybook/web-components-vite'
-import path from 'path';
+import path from 'path'
 
 const config: StorybookConfig = {
   stories: ['../**/*.mdx', '../**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions', '@chromatic-com/storybook'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    '@chromatic-com/storybook',
+  ],
   framework: {
     name: '@storybook/web-components-vite',
     options: {},
@@ -13,18 +18,18 @@ const config: StorybookConfig = {
   },
   async viteFinal(config, options) {
     // Ensures that the cache directory is inside the project directory
-    config.cacheDir = path.join(__dirname, '../node_modules/.vite-storybook');
+    config.cacheDir = path.join(__dirname, '../node_modules/.vite-storybook')
     config.resolve = {
       ...config.resolve,
       alias: {
-        "@": path.resolve(__dirname, "../"),
+        '@': path.resolve(__dirname, '../'),
       },
-    };
+    }
     config.define = {
       ...config.define,
-      "process.env": process.env,
-    };
-    return config;
+      'process.env': process.env,
+    }
+    return config
   },
 }
 export default config
--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,9 +1,9 @@
 import type { Preview } from '@storybook/web-components'
 
-const personSchemaWrapper = (content) => {
+const personSchemaWrapper = content => {
   return `<article itemscope itemtype="https://schema.org/Person">${content}</article>`
 }
-global.psw = personSchemaWrapper;
+global.psw = personSchemaWrapper
 
 const preview: Preview = {
   parameters: {
